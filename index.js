fb44751e6ac20840091531fd4e3bc91294ac1258
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-04-22 22:59:58 +0200

    added soundex compare
    Should now compare a keyword with soundex incase of mistyping.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const JsonHandler = require('./data/JsonHandler.js');
 const Parse = require('./components/parseMessage.js');
 const getRandomInt = require('./components/getRandomInt.js');
 const HueController = require('./components/hue.js');
+const soundCompare = require('./components/keywordCompare.js');
 
 const client = new Discord.Client();
 const data = new JsonHandler();
@@ -11,22 +12,30 @@ const parse = new Parse();
 const hue = new HueController();
 
 client.on('ready', () => {
-  client.user.setPresence({ status: 'online', game: { name: 'v3.0.4', type: 'WATCHING' } });
+  client.user.setPresence({ status: 'online', game: { name: 'v3.0.5', type: 'WATCHING' } });
   console.log('bot ready');
   console.log('---------------------');
 });
 
 client.on('message', async (message) => {
   try {
+    // this is where the keywords with ! at the begining is handled.
     if (message.content.startsWith('!')) {
-      console.log(message.content);
+      console.log('got message: ', message.content);
       // get the keywords from the datafile, parse the message for the keyword.
       let keywords = await data.getKeywords();
       let keyword = await parse.get(message.content);
       keyword = keyword.toLowerCase();  // make the keyword lowercase
-      console.log(keywords[keyword]);
+
+      if (keywords[keyword] === undefined) {
+        console.log('given keyword is undefined, matching soundex');
+        // check for similarities using soundex
+        // if there is a match just set the keyword to the property from the keywords object.
+        keyword = soundCompare(keyword, keywords);
+      }
 
       if (keywords[keyword]) {
+        console.log('getting random item');
         let keywordarr = keywords[keyword];         // create an array of the keyword data
         let rand = getRandomInt(keywordarr.length); // get a random int for indexing the array
 
@@ -34,6 +43,7 @@ client.on('message', async (message) => {
       }
     }
 
+    // TODO: make the add keyword take multi line. Seems like my regex can't parse multiline.
     if (message.content.startsWith('+add')) {
       let add = await parse.add(message.content);
       data.addKeyword(add[1].toLowerCase(), add[2]);
@@ -47,6 +57,7 @@ client.on('message', async (message) => {
       data.getKeywords();
     }
 
+    // TODO: change the way we get ligths with api.lights instead of api.getLights.
     if (message.content.startsWith('.hue')) {
       let users = await data.getUsers();
       if (users[message.author.id] === 'admin') {
@@ -67,7 +78,7 @@ client.on('message', async (message) => {
           }
           message.channel.send(reply);
         }
-  
+
         if (cmd[1] == 'set') {
           try {
             let state;
