f04caec990988b79d17291f320927e85a683fc78
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-04-27 01:10:17 +0200

    fix if and reply

--- index.js
+++ index.js
@@ -112,9 +112,9 @@ client.on('message', async (message) => {
 
     if (message.content.startsWith('.wol')) {
       let user = data.getUser(message.author.id);
-      if ((user.type === 'admin') || (user.mac)) {
+      if ((user.type === 'admin') && (user.mac)) {
         let res = await wol(user.mac);
-        if (res) message.channel.send('sent magic packet to IF' + mac);
+        if (res) message.channel.send('sent magic packet to IF' + user.mac);
       }
       else {
         // user is not admin or doesnt have mac
