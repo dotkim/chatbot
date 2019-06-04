2b6bb1589d81f6e7df167c0bda3e6d782ed46d57
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-04 17:38:48 +0200

    changed thumbnail lib
    image-thumbnail made some of the thumbs way too small.

--- components/thumbnail.js
+++ components/thumbnail.js
@@ -1,16 +1,16 @@
 require('dotenv').config();
-const imageThumbnail = require('image-thumbnail');
+const sharp = require('sharp');
 const fs = require('fs');
 
 const path = process.env.FILEPATH;
 const thumbpath = process.env.THUMBPATH;
-let options = { width: 200 }
+let options = { width: 200, fit: 'inside' };
 
 module.exports = async function (name) {
   try {
     let file = path + name;
     let thumbfile = thumbpath + name;
-    const thumbnail = await imageThumbnail(file, options);
+    const thumbnail = await sharp(file).resize(options).toBuffer();
     fs.writeFileSync(thumbfile, thumbnail);
   }
   catch (error) {
