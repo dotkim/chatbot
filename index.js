ae3cfcc00640523e7092f9c7ced56360105048fc
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-06-07 23:37:00 +0200

    3.4.3

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const images = new imageHandler();
 const fetch = new ImgFetch();
 
 client.on('ready', () => {
-  client.user.setPresence({ status: 'online', game: { name: 'v3.4.2 ".help"', type: 'WATCHING' } });
+  client.user.setPresence({ status: 'online', game: { name: 'v3.4.3 ".help"', type: 'WATCHING' } });
   console.log('bot ready');
   console.log('---------------------');
 });
