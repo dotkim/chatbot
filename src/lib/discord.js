0ed2cb05baefc7066122b043cd12c966848f41e7
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-10-30 23:31:51 +0100

    added constructor and login function

--- src/lib/discord.js
+++ src/lib/discord.js
@@ -1,3 +1,30 @@
+const Discord = require('discord.js');
+const config = require('../config/configuration');
+
 module.exports = class {
-  
+  /**
+   * creates a new instance of the discord client
+   * and sets it to this.client
+   * @constructor
+   */
+  constructor() {
+    this.client = new Discord.Client();
+    return this.client;
+  }
+
+  /**
+   * creates the bot session
+   * uses the token from config
+   * if token is undefined (default) the app quits
+   * returns void
+   */
+  login() {
+    if (!config.token) {
+      console.error('Discord token is undefined, please provide a token');
+      process.exit(1);
+    }
+    else {
+      client.login(config.token);
+    }
+  }
 }
