0cf21d1311a296a35e4e324d3b3c73e10261bfc2
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-03 16:11:25 +0100

    added post method

--- src/api/requestFactory.js
+++ src/api/requestFactory.js
@@ -36,3 +36,43 @@ module.exports = class {
     const url = `${config.apiUrl}/${this.route}${query}`
     return new URL(url);
   }
+
+  /**
+   * build a option object for a post request
+   * @param {Number} bodyLength - the lenght of the body object sent to the api
+   * 
+   * returns a 'options' object for the request
+   */
+  post(bodyLength) {
+    if (!bodyLength) return new Error('bodyLength parameter is missing');
+    if (typeof bodyLength != 'Number') return new Error('bodyLength parameter must be a Number');
+    
+    const querystring = require('querystring');
+
+    const query = querystring.stringify(this.parameters);
+    const url = `${config.apiUrl}/${this.route}?${query}`
+    const auth = new Buffer.from(config.apiAuth).toString('base64');
+
+    let options = {};
+
+    //build options object
+    options.host   = url.host;
+    options.method = this.method;
+    options.path   = url.pathname;
+
+    //options.protocol
+    url.startsWith('https')
+    ? options.protocol = 'https'
+    : options.protocol = 'http';
+
+    //options.port
+    if (url.port != '') options.port = url.port;
+
+    //options.headers
+    options.headers = {
+      'Content-Type': 'application/json',
+      'Content-Length': bodyLength,
+      'Authorization': 'Basic ' + auth
+    }
+  }
+}
