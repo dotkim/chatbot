af5db95f2b62b33ce7e9cb0f0d1e9ab00be8ae09
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-04 18:15:30 +0100

    changed config to a class, removed gimmicky function

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -3,28 +3,29 @@ require('dotenv').config();
 
 /**
  * returns a config object
- * 
+ *
  * reads the .env file for the config
  * it sets the default value if nothing is found for that key
- * @function
+ * @class
  * @exports Config
  */
-function Config() {
-  this.config = {};
-  //Discord bot token
-  this.config.token = process.env.TOKEN || undefined;
-
-  //if a domain name should be excluded from the image sniffer
-  this.config.ignoredDomain = process.env.IGNOREDDOMAIN || undefined;
-
-  //API info
-  //If an URL is provided, the bot will send images to that API
-  //The API description is not done yet, check https://github.com/dotkim/images-api
-  //the auth is set on the API, currently only Basic auth
-  this.config.apiUrl  = process.env.APIURL  || undefined;
-  this.config.apiPort = process.env.APIPORT || 80;
-  this.config.apiAuth = process.env.APIAUTH || 'admin:secret';
+class Config {
+  constructor() {
+    this.config = {};
+    //Discord bot token
+    this.config.token = process.env.TOKEN || undefined;
+    //if a domain name should be excluded from the image sniffer
+    this.config.ignoredDomain = process.env.IGNOREDDOMAIN || undefined;
+    //API info
+    /*
+     *If an URL is provided, the bot will send images to that API
+     * The API description is not done yet, check https://github.com/dotkim/images-api
+     * The auth is set on the API, currently only Basic auth
+     */
+    this.config.apiUrl = process.env.APIURL || undefined;
+    this.config.apiPort = process.env.APIPORT || 80;
+    this.config.apiAuth = process.env.APIAUTH || 'admin:secret';
+  }
 }
 
-Config.call(this);
-module.exports = this.config;
+module.exports = new Config().config;
