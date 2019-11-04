684373b20d9d2471851c9cc4d69462e399a9cb18
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-04 19:13:44 +0100

    jsdoc

--- src/api/keywords.js
+++ src/api/keywords.js
@@ -2,12 +2,29 @@
 const Request = require('./request');
 const route = 'keywords'
 
+/**
+ * Class for getting and adding keywords
+ * @class
+ */
 class Keywords {
+  /**
+   * Get a specific keyword from the API
+   * @param {String} keyword - The keyword to get from the API
+   * 
+   * Returns a promise with the keyword object
+   */
   get(keyword) {
     let req = new Request(route, keyword);
     return req.get()
   }
 
+  /**
+   * Add a new or update an existing keyword
+   * @param {String} keyword - The new or existing keyword
+   * @param {String} message - The message to add
+   * 
+   * Returns a promise with the new keyword object
+   */
   add(keyword, message) {
     let req = new Request(route, keyword, {message});
     return req.post();
