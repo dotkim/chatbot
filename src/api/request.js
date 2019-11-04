12aa833af731747aae63fd869240b54dc58578e2
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-04 15:41:37 +0100

    Create request.js

---
+++ src/api/request.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const RequestFactory = require('./requestFactory');
+const requestBody = require('./requestBody');
+
+/**
+ * Request module for getting and posting
+ * @class
+ */
+class Request {
+  constructor(route, param, body = {}) {
+    if (body.length > 0) {
+      this.body = requestBody(body);
+      this.factory = new RequestFactory(route, param, 'POST');
+      this.options = this.factory.post(body.length);
+    } else {
+      this.factory = new RequestFactory(route, param);
+      this.url = this.factory.get();
+    }
+  }
+
+  /**
+   * Send a GET request to the received URL
+   * Returns a promise with data
+   */
+  get() {
+    return new Promise((resolve, reject) => {
+      const lib = this.options.protocol == 'https'
+        ? require('https')
+        : require('http');
+
+      const request = lib.get(this.url, (response) => {
+        if (response.statusCode < 200 || response.statusCode > 299) {
+          reject(new Error('Failed to load page, status code: ' + response.statusCode));
+        }
+        const body = [];
+        response.on('data', (chunk) => body.push(chunk));
+        response.on('end', () => resolve(JSON.parse(body.join(''))));
+      });
+
+      request.on('error', (err) => reject(err))
+    });
+  }
+
+  /**
+   * Send a POST request to the received URL
+   * Returns a promise with data
+   */
+  post() {
+    return new Promise((resolve, reject) => {
+      const lib = this.options.protocol == 'https'
+        ? require('https')
+        : require('http');
+
+      const request = lib.request(this.options, (response) => {
+
+        if (response.statusCode < 200 || response.statusCode > 299) {
+          reject(new Error('Failed to load page, status code: ' + response.statusCode));
+        }
+
+        const returnBody = [];
+
+        response.on('data', (chunk) => returnBody.push(chunk));
+        response.on('end', () => resolve(JSON.parse(returnBody.join(''))));
+      });
+
+      request.on('error', (err) => reject(err))
+
+      request.write(this.body);
+      request.end();
+    });
+  }
+}
+
+module.exports = Request;
