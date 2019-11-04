b6007536aee7ff8d2d5849007e5683fe857a64f0
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-04 15:42:27 +0100

    Fixed ESlint error, spaced-comment

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -11,16 +11,16 @@ require('dotenv').config();
  */
 function Config() {
   this.config = {};
-  // Discord bot token
+  //Discord bot token
   this.config.token = process.env.TOKEN || undefined;
 
-  // if a domain name should be excluded from the image sniffer
+  //if a domain name should be excluded from the image sniffer
   this.config.ignoredDomain = process.env.IGNOREDDOMAIN || undefined;
 
-  // API info
-  // If an URL is provided, the bot will send images to that API
-  // The API description is not done yet, check https://github.com/dotkim/images-api
-  // the auth is set on the API, currently only Basic auth
+  //API info
+  //If an URL is provided, the bot will send images to that API
+  //The API description is not done yet, check https://github.com/dotkim/images-api
+  //the auth is set on the API, currently only Basic auth
   this.config.apiUrl  = process.env.APIURL  || undefined;
   this.config.apiPort = process.env.APIPORT || 80;
   this.config.apiAuth = process.env.APIAUTH || 'admin:secret';
