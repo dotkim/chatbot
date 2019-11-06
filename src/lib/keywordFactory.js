3cb2444dbd0dec73c08290d3b494c3ba5120d451
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-06 22:06:55 +0100

    changed methods to match the new request changes
    Also some documenting

--- src/lib/keywordFactory.js
+++ src/lib/keywordFactory.js
@@ -1,6 +1,6 @@
 'use strict'
 const Request = require('../api/request');
-const keywordSchema = require('../models/keywordSchema');
+const Keyword = require('./keyword');
 
 /**
  * Class for building keyword objects
@@ -9,36 +9,57 @@ const keywordSchema = require('../models/keywordSchema');
 class KeywordFactory {
   /**
    * @constructor
+   * @param {String} [route='keyword'] - The API route
+   * @example
+   * const KeywordFactory = require('lib/keywordFactory');
+   * const factory = new KeywordFactory();
    */
-  constructor() {
-    this.route = 'keyword'
+  constructor(route='keyword') {
+    this.route = route;
   }
 
   /**
    * Add a new or update an existing keyword
-   * @param {String|Array<String>} keyword - The new or existing keyword
+   * @param {String|String[]} keyword - The new or existing keyword
    * @param {String} message - The message to add
-   * @returns {Promise<Object>} Promise object represents a new or updated keyword object
+   * @returns {Promise<Keyword>} Promise object represents a new or updated keyword object
    * @example
-   * const KeywordFactory = require('lib/keywordFactory');
-   * const factory = new KeywordFactory();
    * (async function() {
    *   let result = await factory.add('angry', 'I am very angry!!'); // returns added keyword object
    * })();
    */
-  add(keyword, message) {
-    let req = new Request(this.route, keyword, { message });
-    return req.post();
+  async add(keyword, message) {
+    let options = {
+      parameters: keyword,
+      method: 'POST',
+      body: { message }
+    };
+
+    let req = new Request(this.route, options);
+    let res = await req.send();
+
+    return new Keyword(res);
   }
 
   /**
    * Get a specific keyword from the API
    * @param {String} keyword - The keyword to get from the API
-   * @returns {Promise<Object>} Promise object represents a keyword object
+   * @returns {Promise<Keyword>} Promise object represents a keyword object
+   * @example
+   * (async function() {
+   *   let result = await factory.get('angry');
+   * })();
    */
-  get(keyword) {
-    let req = new Request(this.route, keyword);
-    return req.get()
+  async get(keyword) {
+    let options = {
+      parameters: keyword,
+      method: 'AUTHGET'
+    };
+
+    let req = new Request(this.route, options);
+    let res = await req.send();
+
+    return new Keyword(res);
   }
 }
 
