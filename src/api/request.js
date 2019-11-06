03fb971c51eba5da4aa7eba12c8ce90240cdfc2b
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-06 22:04:26 +0100

    variable renaming because of new factory return

--- src/api/request.js
+++ src/api/request.js
@@ -38,11 +38,11 @@ class Request {
    */
   send() {
     return new Promise((resolve, reject) => {
-      const lib = this.options.protocol == 'https'
+      const lib = this.options.urlObj.protocol == 'https:'
         ? require('https')
         : require('http');
 
-      const request = lib.request(this.options, (response) => {
+      const request = lib.request(this.options.urlObj, this.options.options, (response) => {
 
         if (response.statusCode < 200 || response.statusCode > 299) {
           reject(new Error('Failed to load page, status code: ' + response.statusCode));
