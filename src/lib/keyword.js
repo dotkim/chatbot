06f235caa72bc64c80e61f2105f0d71bf63b7dac
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-06 22:04:54 +0100

    Create keyword.js

---
+++ src/lib/keyword.js
@@ -0,0 +1,24 @@
+'use strict'
+const keywordSchema = require('../models/keywordSchema');
+
+/**
+ * Creates a Keyword
+ * @class
+ */
+class Keyword {
+  /**
+   * Sets the object values to a schema object
+   * @constructor
+   * @param {Object} responseObject - The object received from the request response
+   * @example
+   * const Keyword = require('keyword');
+   * const keyword = new Keyword({ values: ['I am very angry!!'], keyword: 'angry' });
+   */
+  constructor(responseObject) {
+    this.schema = keywordSchema;
+    this.schema.keyword = responseObject.keyword;
+    this.schema.values = responseObject.values;
+  }
+}
+
+module.exports = Keyword;
