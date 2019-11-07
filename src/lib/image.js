de38112e8ab654acd4c5de4e8c8db9687a1eb960
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-07 15:06:28 +0100

    redid image constructors

---
+++ src/lib/image.js
@@ -0,0 +1,25 @@
+'use strict'
+const imageSchema = require('../models/imageSchema');
+
+/**
+ * Creates an Image
+ * @class
+ */
+class Image {
+  /**
+   * Sets the object values to a schema object
+   * @constructor
+   * @param {Object} responseObject - The object received from the request response
+   * @example
+   * const Image = require('image');
+   * const image = new Image({ fileName: '1234.jpg', url: 'http://example.com/1234.jpg' });
+   */
+  constructor(responseObject) {
+    this.schema = imageSchema;
+    Object.keys(this.schema).forEach((key) => {
+      this.schema[key] = responseObject[key];
+    });
+  }
+}
+
+module.exports = Image;
--- src/lib/imageFactory.js
+++ src/lib/imageFactory.js
@@ -1,48 +1,58 @@
 'use strict'
 const Request = require('../api/request');
-const imageSchema = require('../models/imageSchema');
+const Image = require('./image');
 
 /**
- * Class for building image objects
+ * Class for building image objects.
  * @class
  */
 class ImageFactory {
   /**
    * @constructor
-   * @param {String} [route='images'] - API route
+   * @param {String} [route='images'] - API route.
+   * @example
+   * const ImageFactory = require('lib/imageFactory');
+   * const factory = new ImageFactory();
    */
   constructor(route = 'images') {
     this.route = route;
   }
 
   /**
-   * Insert an image to the API
-   * @param {String|Array<String>} param - Path parameters
-   * @param {Object} image - image object with name and data
-   * @returns {Promise<Object>} Promise object represents a new or updated image object
+   * Insert an image to the API.
+   * @param {String|Array<String>} param - Path parameters.
+   * @param {Object} image - Image object with name and data.
+   * @returns {Promise<Image>} Promise object represents a new or updated image object.
    */
-  insert(param, image) {
+  async insert(param, image) {
+    this.options = {};
+    this.options.parameters = param;
+    this.options.body = { image };
+    this.options.method = 'POST';
+
     let req = new Request('insert', param, { image });
-    return req.send();
+    let res = await req.send();
+
+    return new Image(res);
   }
 
   /**
-   * Get a random or a specific image from the API
-   * @param {String|Array<String>} param - Path parameters
-   * @returns {Promise<Image>} Promise object represents an image object
+   * Get a random or a specific image from the API.
+   * @param {String} param - Path parameters.
+   * @returns {Promise<Image>} Promise object represents an image object.
+   * @example
+   * (async function() {
+   *   let result = await factory.get();
+   * })();
    */
   async get(param='getRandom') {
-    let req = new Request(this.route, param);
-    let keys = Object.keys(imageSchema);
-    let data = await req.send();
+    this.options = {};
+    this.options.parameters = param;
 
-    let returnObject = {};
-    
-    keys.forEach((key) => {
-      returnObject[key] = data[key];
-    });
+    let req = new Request(this.route, this.options);
+    let res = await req.send();
 
-    return returnObject;
+    return new Image(res);
   }
 }
 
