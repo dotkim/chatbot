d7bdb270f21da4da703e8c0a59732e185e71a243
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-14 22:35:58 +0100

    jsdoc

--- src/lib/commandFactory.js
+++ src/lib/commandFactory.js
@@ -2,13 +2,22 @@
 const Command = require('./command');
 const CommandParser = require('./commandParser');
 
+/**
+ * Class for building command objects, objects are used for passing a command into another class
+ */
 class CommandFactory {
+  /**
+   * Parse the message for a command.
+   * @param {String} content - Content from the discord message, usually message.content
+   * @returns {Command} represents a new instance of a command, this has a schema property with the command schema.
+   */
   parse(content) {
     const parse = CommandParser(content);
     const command = {
       name: parse.groups.name,
       prefix: parse.groups.prefix
     };
+
     if (!command) throw new Error('Command was not parsed correctly');
 
     return new Command(command);
--- src/lib/commandParser.js
+++ src/lib/commandParser.js
@@ -1,5 +1,10 @@
 'use strict'
 
+/**
+ * Component parser for commands, this uses a regex to check if the beginning of the message has a prefix.
+ * @param {String} message - Content from the discord message.
+ * @returns {Object<RegExpExecArray>} Represents an executed regex string, containing the name and prefix properties.
+ */
 const CommandParser = function CommandParser(message) {
   const regex = /^(?<prefix>.)(?<name>\w+)$/giu;
   return regex.exec(message);
