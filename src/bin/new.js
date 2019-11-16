af62d8b4f20fd2d9682105a379478c853c88da8d
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-11-16 13:44:24 +0100

    more testing with a new index file
    Still want to spread what it does, just need to figure out how.

--- src/bin/new.js
+++ src/bin/new.js
@@ -1,7 +1,9 @@
 'use strict'
 //THIS FILE WILL CHANGE NAME TO INDEX.JS WHEN THE OTHER FILE HAS BEEN REMOVED
 
-const CommandFactory = require('../lib/commandFactory');
+const CommandFactory = require('../api/commandFactory');
+const PrefixChecker = require('../config/prefixes');
+let Factory = '';
 
 /*
  * Discord functionality might be moved to an initilize component later.
@@ -10,28 +12,40 @@ const CommandFactory = require('../lib/commandFactory');
 const Discord = require('../lib/discord');
 
 const bot = new Discord();
-const factory = new CommandFactory();
+const cmdfactory = new CommandFactory();
 
 bot.ready(() => {
   bot.setPresence('test');
 });
 
-bot.message((message) => {
+bot.message(async (message) => {
   //eslint-disable-next-line no-console
   console.log(message.content);
   if (bot.isBot(message)) return;
 
-  const command = factory.parse(message.content);
+  const command = cmdfactory.parse(message.content);
+  //eslint-disable-next-line no-unused-vars
+  const aaaa = PrefixChecker(command.schema.prefix);
 
-  bot.send(message, JSON.stringify(command.schema)).
-  then((res) => {
+  const availCmds = [
+    'keyword',
+    'image'
+  ]
+
+  if (availCmds.includes(aaaa)) Factory = require(`../api/${aaaa}Factory`);
+
+  const factory = new Factory();
+  let keyword = {};
+
+  try {
+    keyword = await factory.get(command.schema.name);
+  } catch (err) {
     //eslint-disable-next-line no-console
-    console.log(res);
-  }).
-  catch(() => {
-    //eslint-disable-next-line no-console
-    console.error();
-  })
-})
+    console.error(err);
+  }
+
+  //eslint-disable-next-line no-console
+  console.log(keyword);
+});
 
 bot.login();
