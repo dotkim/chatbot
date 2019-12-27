bff4d0d83fbe0f90556c5d3a3bafd07e231a7a0a
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2019-12-27 13:37:56 +0100

    Update Worker.js

--- src/lib/Worker.js
+++ src/lib/Worker.js
@@ -1,7 +1,27 @@
 'use strict'
+const CommandFactory = require('../api/CommandFactory');
+const PrefixChecker = require('../config/prefixes');
+const cmdfactory = new CommandFactory();
 
 class Worker {
+  constructor(bot) {
+    this.bot = bot;
+  }
 
+  ParseMessage(message) {
+    if (this.bot.isBot(message)) return;
+    try {
+      const command = cmdfactory.parse(message.content);
+      const factoryPrefix = PrefixChecker(command.prefix);
+      const Factory = require(`../api/${factoryPrefix}Factory`);
+      const factory = new Factory();
+      const data = factory.get(command.name);
+      return data;
+    } catch (err) {
+      //eslint-disable-next-line no-console
+      console.error(err);
+    }
+  }
 }
 
 module.exports = Worker;
