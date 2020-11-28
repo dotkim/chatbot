77e559269e2bcf45100a59a1c38f3eddd4214c42
Kim Nerli <32514438+dotkim@users.noreply.github.com>
2020-11-28 19:50:07 +0100

    spacing for readabilty

--- Types/Configuration.cs
+++ Types/Configuration.cs
@@ -3,11 +3,14 @@ namespace ChatBot.Types
   public class Configuration
   {
     public string Token { get; set; }
+
     public string DatabasePath { get; set; }
+
     public string ApiUrl { get; set; }
     public string RandomService { get; set; }
     public string CryService { get; set; }
     public string ExcludeService { get; set; }
+    
     public string ApiUsername { get; set; }
     public string ApiSecret { get; set; }
   }
