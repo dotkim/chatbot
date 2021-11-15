cc6357621c8b765c18cbe0ceea020ec9c5fa250d
dotkim <kim.andre.nerli@gmail.com>
2021-11-15 17:22:04 +0000

    Added summary to method

--- ChatBot/Libraries/RegexHelper.cs
+++ ChatBot/Libraries/RegexHelper.cs
@@ -4,6 +4,11 @@ namespace ChatBot.Libraries
 {
   public static class RegexHelper
   {
+    /// <summary>
+    /// Check if a string is an URL.
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
     public static string Url(string input)
     {
       string pattern = @"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)";
